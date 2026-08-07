0889363c7efc43e314d33b287365b6a80f6136f8
I'm Dang Khoa <khoablackcode407@gmail.com>
2026-08-07 14:02:07 +0700

    feat: add PM2 ecosystem config for frontend and backend

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: "garden-backend",
+      script: "./server/index.js",
+      watch: false,
+      env: {
+        NODE_ENV: "production",
+        PORT: 5000
+      }
+    },
+    {
+      name: "garden-frontend",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",
+      watch: false,
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
